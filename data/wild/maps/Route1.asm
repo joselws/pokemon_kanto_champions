0a09288838fa945a3bf584f9fc2a8e5e69ae3116
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-04 07:08:07 -0400

    rearrange route1 percents

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -4,12 +4,12 @@ Route1WildMons:
 	db  5, PIDGEY
 	db  4, RATTATA
 	db  4, PIDGEY
-	db  3, PIDGEY
-	db  3, RATTATA
-	db  2, RATTATA
-	db  2, PIDGEY
-	db  3, PIDGEY
-	db  3, PIDGEY
+	db  4, PIDGEY
+	db  5, RATTATA
+	db  4, RATTATA
+	db  4, PIDGEY
+	db  4, PIDGEY
+	db  5, PIDGEY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
