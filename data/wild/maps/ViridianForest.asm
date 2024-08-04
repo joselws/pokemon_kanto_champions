0337d9671f4a4702cfa6063e20ac9eeebb2b103f
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-04 12:20:15 -0400

    viridian forest encounters

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,14 +1,16 @@
 ViridianForestWildMons:
 	def_grass_wildmons 8 ; encounter rate
 IF DEF(_RED)
-	db  4, WEEDLE
-	db  5, KAKUNA
-	db  3, WEEDLE
+	db  5, CATERPIE
 	db  5, WEEDLE
-	db  4, KAKUNA
-	db  6, KAKUNA
-	db  4, METAPOD
-	db  3, CATERPIE
+	db  7, KAKUNA
+	db  7, METAPOD
+	db  5, PIKACHU
+	db  5, PIKACHU
+	db  6, WEEDLE
+	db  6, CATERPIE
+	db  8, METAPOD
+	db  8, KAKUNA
 ENDC
 IF DEF(_BLUE)
 	db  4, CATERPIE
@@ -19,9 +21,9 @@ IF DEF(_BLUE)
 	db  6, METAPOD
 	db  4, KAKUNA
 	db  3, WEEDLE
-ENDC
-	db  3, PIKACHU
 	db  5, PIKACHU
+	db  5, PIKACHU
+ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
