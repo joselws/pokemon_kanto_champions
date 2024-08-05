4539545a3f859a3925db2e68577109ab06313581
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-05 07:51:20 -0400

    mtmoon encounters

--- data/wild/maps/MtMoonB1F.asm
+++ data/wild/maps/MtMoonB1F.asm
@@ -1,15 +1,15 @@
 MtMoonB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  8, ZUBAT
-	db  7, ZUBAT
-	db  7, GEODUDE
-	db  8, GEODUDE
-	db  9, ZUBAT
-	db 10, PARAS
-	db 10, ZUBAT
-	db 11, ZUBAT
-	db  9, CLEFAIRY
-	db  9, GEODUDE
+	db  13, ZUBAT
+	db  12, ZUBAT
+	db  13, GEODUDE
+	db  14, GEODUDE
+	db  14, ZUBAT
+	db  13, PARAS
+	db  13, ZUBAT
+	db  13, ZUBAT
+	db  13, CLEFAIRY
+	db  13, GEODUDE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
