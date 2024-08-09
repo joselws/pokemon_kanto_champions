f698b2c7d8a1bc850a716dc58589bc0bba42aaae
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-09 17:58:08 -0400

    route 25 reworked

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,29 +1,26 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
-	db  9, KAKUNA
-	db 13, PIDGEY
-	db 12, ODDISH
-	db 13, ODDISH
-	db 12, ABRA
-	db 14, ODDISH
-	db 10, ABRA
-	db  7, METAPOD
-	db  8, CATERPIE
+	db 17, BEEDRILL
+	db 17, BUTTERFREE
+	db 18, PIDGEOTTO
+	db 17, ODDISH
+	db 17, BELLSPROUT
+	db 15, ABRA
+	db 17, ODDISH
 ENDC
 IF DEF(_BLUE)
-	db  8, CATERPIE
-	db  9, METAPOD
-	db 13, PIDGEY
+	db  7, CATERPIE
+	db  8, METAPOD
+	db 12, PIDGEY
 	db 12, BELLSPROUT
 	db 13, BELLSPROUT
-	db 12, ABRA
-	db 14, BELLSPROUT
 	db 10, ABRA
-	db  7, KAKUNA
-	db  8, WEEDLE
+	db 14, BELLSPROUT
 ENDC
+	db 18, BELLSPROUT
+	db 15, ABRA
+	db 15, ABRA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
