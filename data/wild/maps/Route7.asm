fbdcb78e56945dc992b730c9842f8726b00c9aca
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-11 14:03:01 -0400

    route 7 reworked

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -2,15 +2,15 @@ Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 19, PIDGEY
 IF DEF(_RED)
-	db 19, ODDISH
-	db 17, MANKEY
-	db 22, ODDISH
-	db 22, PIDGEY
-	db 18, MANKEY
-	db 18, GROWLITHE
-	db 20, GROWLITHE
-	db 19, MANKEY
-	db 20, MANKEY
+	db 30, MANKEY
+	db 31, EKANS
+	db 32, VULPIX
+	db 30, GROWLITHE
+	db 30, PIDGEOTTO
+	db 31, VULPIX
+	db 32, GROWLITHE
+	db 31, MANKEY
+	db 32, ARBOK
 ENDC
 IF DEF(_BLUE)
 	db 19, BELLSPROUT
