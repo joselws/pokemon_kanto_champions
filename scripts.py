2a52e687689cddc394d5b364f84a75ac704444ba
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-15 08:15:36 -0400

    random team script

--- scripts.py
+++ scripts.py
@@ -1,3 +1,12 @@
+from random import choice
+from constants import LAST_STAGE_POKEMONS
+
+
 def avg(*args) -> int:
     average = sum(args) // len(args)
     return average
+
+
+def random_team() -> list[str]:
+    chosen_pokemon = [choice(LAST_STAGE_POKEMONS) for _ in range(6)]
+    return chosen_pokemon
