0d047cf6b1d93648482599b9b73020ab04a0b90b
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-19 21:28:00 -0400

    modified random_team script

--- scripts.py
+++ scripts.py
@@ -8,5 +8,5 @@ def avg(*args) -> int:
 
 
 def random_team() -> list[str]:
-    chosen_pokemon = [choice(LAST_STAGE_POKEMONS).upper() for _ in range(6)]
-    print(f"{chosen_pokemon[0]}, {chosen_pokemon[1]}, {chosen_pokemon[2]}, {chosen_pokemon[3]}, {chosen_pokemon[4]}, {chosen_pokemon[5]}")
+    pkmns = [choice(LAST_STAGE_POKEMONS).upper() for _ in range(6)]
+    print(f"{pkmns[0]}, {pkmns[1]}, {pkmns[2]}, {pkmns[3]}, {pkmns[4]}, {pkmns[5]}")
