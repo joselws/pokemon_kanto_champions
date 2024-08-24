60c1f46cde6de43ed289953910b68ff2f6094f8c
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-08-24 17:58:34 -0400

    route 23 rework

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,25 +1,25 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 26, EKANS
+	db 65, ARBOK
 ENDC
 IF DEF(_BLUE)
 	db 26, SANDSHREW
 ENDC
-	db 33, DITTO
-	db 26, SPEAROW
-	db 38, FEAROW
-	db 38, DITTO
-	db 38, FEAROW
+	db 65, DITTO
+	db 65, SANDSLASH
+	db 65, FEAROW
+	db 65, DITTO
+	db 65, FEAROW
 IF DEF(_RED)
-	db 41, ARBOK
+	db 65, ARBOK
 ENDC
 IF DEF(_BLUE)
 	db 41, SANDSLASH
 ENDC
-	db 43, DITTO
-	db 41, FEAROW
-	db 43, FEAROW
+	db 68, DITTO
+	db 68, FEAROW
+	db 68, FEAROW
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
